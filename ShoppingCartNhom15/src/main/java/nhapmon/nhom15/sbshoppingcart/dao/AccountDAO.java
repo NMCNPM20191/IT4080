74692f78e15bdad72980145decdb9e3bb728d8eb
Hauham <phuchauzalo@gmail.com>
2019-11-05 23:38:31 +0700

    Create project

---
+++ ShoppingCartNhom15/src/main/java/nhapmon/nhom15/sbshoppingcart/dao/AccountDAO.java
@@ -0,0 +1,23 @@
+package nhapmon.nhom15.sbshoppingcart.dao;
+ 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import nhapmon.nhom15.sbshoppingcart.entity.Account;
+ 
+@Transactional
+@Repository
+public class AccountDAO {
+ 
+    @Autowired
+    private SessionFactory sessionFactory;
+ 
+    public Account findAccount(String userName) {
+        Session session = this.sessionFactory.getCurrentSession();
+        return session.find(Account.class, userName);
+    }
+ 
+}
